9083954300576a03867c3c25dca1c0e9adb2ffa9
CyberFlame <cyberflameu@gmail.com>
2021-08-16 12:27:32 +1200

    rename groupid
    this was just copy-pasted from another boilerplate

--- src/main/java/net/cyberflame/plugin/listeners/PlayerJoinListener.java
+++ src/main/java/net/cyberflame/plugin/listeners/PlayerJoinListener.java
@@ -5,7 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import me.sammwy.example.ExamplePlugin;
+import net.cyberflame.plugin.Plugin;
 
 public class PlayerJoinListener implements Listener {
     @EventHandler
