a6a44498d2ba103e7d411e765b9da27b03e54ade
CyberFlame <cyberflameu@gmail.com>
2021-11-02 12:53:09 +1300

    Update PlayerJoinListener.java

--- src/main/java/net/cyberflame/plugin/listeners/PlayerJoinListener.java
+++ src/main/java/net/cyberflame/plugin/listeners/PlayerJoinListener.java
@@ -5,12 +5,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
-import net.cyberflame.plugin.Plugin;
+import net.cyberflame.plugin.Main;
 
 public class PlayerJoinListener implements Listener {
     @EventHandler
     public void onPlayerJoin (final PlayerJoinEvent event) {
-        final String message = ExamplePlugin.getInstance().getConfig().getString("messages.from-listener");
+        final String message = Main.getInstance().getConfig().getString("messages.from-listener");
         event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', message));
     }
 }
